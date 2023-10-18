e563a621a2500449f6234a6e369ba7d1e62652ff
Simon Dunn <simondunn90@gmail.com>
2023-10-18 16:23:54 +0100

    transition status and withdraw

--- app/routes/applications.js
+++ app/routes/applications.js
@@ -1,5 +1,6 @@
 const Wreck = require('@hapi/wreck')
 const { BASE_URL } = require('../constants/base-url')
+const { WITHDRAW } = require('../constants/abaco-transitions')
 const { WRECK_OPTIONS } = require('../constants/wreck-options')
 const { GET, POST } = require('../constants/http-verbs')
 const { USER } = require('../constants/scopes')
@@ -21,6 +22,42 @@ module.exports = [{
       ...applicationsPerYear.payload.count,
       ...applicationSummary.payload
     }).code(200)
+  }
+},
+{
+  /**
+   * Returns transition status of an application
+   * @param {string} applicationId - unique ID of the application
+   * @return {object} object containing transition status last transition details and available transitions
+   */
+  method: GET,
+  path: '/applications/status/{applicationId}',
+  options: { auth: { strategy: 'simple', scope: [USER] } },
+  handler: async (request, h) => {
+    const applicationStatus = await Wreck.get(`${BASE_URL}/applications/master/api-priv/v1/applications/${request.params.applicationId}/is-in-transition`, WRECK_OPTIONS(request))
+    const availableTransitions = await Wreck.get(`${BASE_URL}/applications/master/api-priv/v1/applications/${request.params.applicationId}/transitions`, WRECK_OPTIONS(request))
+    return h.response({
+      ...applicationStatus.payload,
+      availableTransitions: availableTransitions.payload
+    }).code(200)
+  }
+},
+{
+  /**
+   * close an application
+   * @param {string} applicationId - unique ID for the application
+   * @param {object} notes required, set to null if no value
+   * @return {object} object containing the applicationId and applicationCode
+   */
+  method: POST,
+  path: '/applications/close/{applicationId}',
+  options: { auth: { strategy: 'simple', scope: [USER] } },
+  handler: async (request, h) => {
+    const payload = {
+      notes: null
+    }
+    const application = await Wreck.post(`${BASE_URL}/applications/master/api-priv/v1/applications/${request.params.applicationId}/progress/${WITHDRAW}`, WRECK_OPTIONS(request, payload))
+    return h.response({ ...application.payload }).code(200)
   }
 },
 {
